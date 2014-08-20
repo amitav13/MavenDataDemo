8ae79409e426aef95a6950ad7f9e8ee5233238e4
amitav13 <amitav.khandelwal@gmail.com>
2014-08-19 22:16:13 -0400

    Minor changes to bytesToHuman and floatForm

--- src/com/demo/mavendata/MainActivity.java
+++ src/com/demo/mavendata/MainActivity.java
@@ -53,7 +53,13 @@ public class MainActivity extends Activity {
 	 * A placeholder fragment containing a simple view.
 	 */
 	public static class PlaceholderFragment extends Fragment {
-
+		
+		private static final long Kb = 1  * 1024;
+		private static final long Mb = Kb * 1024;
+		private static final long Gb = Mb * 1024;
+		private static final long Tb = Gb * 1024;
+		private static final long Pb = Tb * 1024;
+		private static final long Eb = Pb * 1024;
 		public PlaceholderFragment() {
 		}
 
@@ -88,28 +94,22 @@ public class MainActivity extends Activity {
 			
 			return rootView;
 		}
+		private static String bytesToHuman (long size)
+	    {
+	        if (size <  Kb)                 return floatForm(        size     ) + " byte";
+	        if (size >= Kb && size < Mb)    return floatForm((double)size / Kb) + " Kb";
+	        if (size >= Mb && size < Gb)    return floatForm((double)size / Mb) + " Mb";
+	        if (size >= Gb && size < Tb)    return floatForm((double)size / Gb) + " Gb";
+	        if (size >= Tb && size < Pb)    return floatForm((double)size / Tb) + " Tb";
+	        if (size >= Pb && size < Eb)    return floatForm((double)size / Pb) + " Pb";
+	        if (size >= Eb)                 return floatForm((double)size / Eb) + " Eb";
+
+	        return "???";
+	    }
+		private static String floatForm (double d)
+	    {
+	       return new DecimalFormat("#.##").format(d);
+	    }
 	}
-	public static String bytesToHuman (long size)
-    {
-        long Kb = 1  * 1024;
-        long Mb = Kb * 1024;
-        long Gb = Mb * 1024;
-        long Tb = Gb * 1024;
-        long Pb = Tb * 1024;
-        long Eb = Pb * 1024;
-
-        if (size <  Kb)                 return floatForm(        size     ) + " byte";
-        if (size >= Kb && size < Mb)    return floatForm((double)size / Kb) + " Kb";
-        if (size >= Mb && size < Gb)    return floatForm((double)size / Mb) + " Mb";
-        if (size >= Gb && size < Tb)    return floatForm((double)size / Gb) + " Gb";
-        if (size >= Tb && size < Pb)    return floatForm((double)size / Tb) + " Tb";
-        if (size >= Pb && size < Eb)    return floatForm((double)size / Pb) + " Pb";
-        if (size >= Eb)                 return floatForm((double)size / Eb) + " Eb";
-
-        return "???";
-    }
-	public static String floatForm (double d)
-    {
-       return new DecimalFormat("#.##").format(d);
-    }
+	
 }
